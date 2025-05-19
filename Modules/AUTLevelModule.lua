b805fe66425a6b26a9f8a26c38307f58aa23debf
Supreme4ab <supreme.4ab@gmail.com>
2025-05-19 20:39:37 +0100

    Update AUTLevelModule.lua

--- Modules/AUTLevelModule.lua
+++ Modules/AUTLevelModule.lua
@@ -1,9 +1,38 @@
 local CommonUtil = loadstring(game:HttpGet("https://raw.githubusercontent.com/Supreme4ab/cassie/main/Main/Modules/CommonUtil.lua"))()
 local Players = CommonUtil.GetService("Players")
 
+-- Config
+
 local AUTLevelUtil = {}
 
---Config
+AUTLevelUtil.TeleportLocations = {
+	["Alabasta"] = Vector3.new(2085.529, 945.397, -2417.796),
+	["Beach"] = Vector3.new(607.062, 917.219, -1989.912),
+	["Boss Spawn Room"] = Vector3.new(19153.26, 910.548, 118.567),
+	["Central Wilds"] = Vector3.new(2528.788, 989.488, -544.948),
+	["Desert Approach"] = Vector3.new(1983.827, 924.261, -1137.514),
+	["Diavolo Spawn"] = Vector3.new(1011.125, 934.362, 2888.195),
+	["Floating Village"] = Vector3.new(1222.878, 1015.637, -281.11),
+	["Football Field"] = Vector3.new(1980.397, 973.423, -356.92),
+	["Goku"] = Vector3.new(2544.647, 916.524, 1828.544),
+	["Guardians Spawn Room"] = Vector3.new(3509.697, 937.592, -430.434),
+	["Gyro TA1"] = Vector3.new(2070.348, 1074.851, -714.886),
+	["Gyro TA2"] = Vector3.new(2272.279, 973.616, 685.383),
+	["Gyro TA4"] = Vector3.new(1947.455, 918.397, -2133.456),
+	["Infernal Cairn"] = Vector3.new(2699.286, 1007.21, -717.92),
+	["Main Subway"] = Vector3.new(2482.364, 973.802, 96.771),
+	["Minos Prime"] = Vector3.new(584.819, 1014.65, -433.61),
+	["OG Sakuya Room"] = Vector3.new(1967.306, 55.113, -1112.305),
+	["Orange Town"] = Vector3.new(-2900.586, 918.631, 15166.34),
+	["Park Center"] = Vector3.new(2075.826, 973.99, 280.726),
+	["Port"] = Vector3.new(2120.274, 921.774, 937.453),
+	["Skaidev"] = Vector3.new(2005.01, 930.397, -2477.821),
+	["Shibuya District"] = Vector3.new(-63.721, 4.6, -9939.011),
+	["Subway"] = Vector3.new(-16788.955, 7.0, -5982.729),
+	["Syrup Spawn"] = Vector3.new(10015.269, -48.41, 30133.928)
+}
+
+-- === Auto-Level Configuration ===
 local ShardRarities = {
 	Common = {
 		"ABILITY_14", "ABILITY_1", "ABILITY_10", "ABILITY_10019", "ABILITY_21", "ABILITY_8881"
@@ -40,12 +69,11 @@ local lastLevel = nil
 AUTLevelUtil.IsFarming = false
 AUTLevelUtil.IsMonitoring = false
 
-local farmThread, levelWatcherThread
+-- functs
 
---Useful Funcs
 function AUTLevelUtil.SetShardRarity(rarity)
-	if AUTLevelUtil.ShardRarities and AUTLevelUtil.ShardRarities[rarity] then
-		AUTLevelUtil.AllowedAbilities = AUTLevelUtil.ShardRarities[rarity]
+	if ShardRarities[rarity] then
+		AUTLevelUtil.AllowedAbilities = ShardRarities[rarity]
 	end
 end
 
@@ -115,8 +143,6 @@ function AUTLevelUtil.RunFarmLoop()
 		farmThread = nil
 	end)
 end
-
---Autofarm
 
 function AUTLevelUtil.RunLevelWatcher(onAscend, onMax)
 	if levelWatcherThread and coroutine.status(levelWatcherThread) ~= "dead" then return end
