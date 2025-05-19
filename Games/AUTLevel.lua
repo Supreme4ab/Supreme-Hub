02ffbc8c03736b4ba2d15b6387b58a1c51956348
Supreme4ab <supreme.4ab@gmail.com>
2025-05-19 21:30:47 +0100

    Update AUTLevel.lua

--- Games/AUTLevel.lua
+++ Games/AUTLevel.lua
@@ -1,3 +1,8 @@
+if getgenv().SunnyDaleHubLoaded then
+    return
+end
+getgenv().SunnyDaleHubLoaded = true
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager      = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
